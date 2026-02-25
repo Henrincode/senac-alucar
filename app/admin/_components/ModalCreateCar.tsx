23e454cc4b890b4ed20e2f7e227a286a20089115
Henrincode <spellctro@gmail.com>
2026-02-25 13:58:01 -0300

    refactor: ModalCreateCar.tsx

--- app/admin/_components/ModalCreateCar.tsx
+++ app/admin/_components/ModalCreateCar.tsx
@@ -11,7 +11,7 @@ export default function ModalCreateCar({ closeModal }: { closeModal: () => void 
     const [work, setWork] = useState(false)
     const [errors, setErrors] = useState<CarModelErrors>({})
 
-    async function create() {
+    async function btnCreate() {
         setWork(true)
         const id_car_category_fk = Number(category)
         const id_car_brand_fk = Number(brand)
@@ -20,26 +20,43 @@ export default function ModalCreateCar({ closeModal }: { closeModal: () => void 
             id_car_category_fk, id_car_brand_fk, name
         }
 
-        const response = await createCarModel(data)
+        const res = await createCarModel(data)
         setWork(false)
-        if (!response.success) {
-            response.errors && setErrors(response.errors)
+        if (!res.success) {
+            res.errors && setErrors(res.errors)
             return
         }
         closeModal()
     }
 
-    function limpar() {
+    function btnClean() {
         setName('')
         setCategory('')
         setBrand('')
     }
+
     return (
         <div onMouseDown={(e) => e.stopPropagation()} className="min-w-100 bg-white">
-            <input onChange={(e) => setName(e.target.value)} value={name} type="text" placeholder="Nome do carro" className={`${errors.name && 'ring-1 ring-red-500'}`} />
-            <input onChange={(e) => setCategory(e.target.value)} value={category} type="text" placeholder="Categoria" className={`${errors.id_car_category_fk && 'ring-1 ring-red-500'}`} />
-            <input onChange={(e) => setBrand(e.target.value)} value={brand} type="text" placeholder="Marca" className={`${errors.id_car_brand_fk && 'ring-1 ring-red-500'}`} />
-            <button disabled={work} onClick={create} type="button" className={`${work && 'text-gray-500'}`}>Cadastrar</button> <button onClick={limpar} type="button">Limpar</button>
+            <input onChange={(e) => setName(e.target.value)}
+                value={name} type="text" placeholder="Nome do carro"
+                className={`${errors.name && 'ring-1 ring-red-500'}`} />
+
+            <input onChange={(e) => setCategory(e.target.value)}
+                value={category} type="text" placeholder="Categoria"
+                className={`${errors.id_car_category_fk && 'ring-1 ring-red-500'}`} />
+
+            <input onChange={(e) => setBrand(e.target.value)}
+                value={brand} type="text" placeholder="Marca"
+                className={`${errors.id_car_brand_fk && 'ring-1 ring-red-500'}`}
+            />
+
+            <button disabled={work} onClick={btnCreate} type="button"
+                className={`${work && 'text-gray-500'}`}>
+                Cadastrar
+            </button>
+            <button onClick={btnClean} type="button">
+                Limpar
+            </button>
         </div>
     )
 }
